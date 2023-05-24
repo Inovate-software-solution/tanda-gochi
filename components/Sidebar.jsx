3e6dbdf40042dae5f2e8fd6407d4028307306195
shamlokhin <shamlokhinau@gmail.com>
2023-05-25 00:47:21 +1000

    delect setting page

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -27,11 +27,7 @@ const Sidebar = ({ children }) => {
             </div>
           </Link>
          
-          <Link href='/setting'>
-            <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
-              <FiSettings size={20} />
-            </div>
-          </Link>
+
           <Link href='/setting'>
             <div className='bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block'>
               <FiLogOut size={20} />
