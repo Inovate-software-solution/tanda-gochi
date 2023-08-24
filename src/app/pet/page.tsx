ffcedfdad37fca408a7a6d3dae9d05d9032977ec
shamlokhin <shamlokhinau@gmail.com>
2023-08-25 03:37:09 +1000

    animation eat

--- src/app/pet/page.tsx
+++ src/app/pet/page.tsx
@@ -1,153 +1,81 @@
-  "use client";
-  import React, { useState, useEffect } from "react";
-  import Sidebar from "../../../components/Sidebar";
-  import Header from "../../../components/Header";
-  import foxGif from "./fox.gif";
-  import standSprite0 from "./sprite_0.png";
-  import standSprite1 from "./sprite_1.png";
-  import moveRightSprite4 from "./sprite_4.png";
-  import moveRightSprite5 from "./sprite_5.png";
-  import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import rightwalkingImage from "../walk.gif";
+import leftwalkingImage from "../walkleft.gif";
+import addOilImage from "../addoil.gif";
+import eatImage from "../eat.gif";
 
-  export default function Page() {
-    const [happiness, setHappiness] = useState(50);
-    const [hunger, setHunger] = useState(50);
-    const [position, setPosition] = useState(0);
-    const [direction, setDirection] = useState("stand");
-    const [currentPet, setCurrentPet] = useState("sprite");
+const WalkingAnimation: React.FC = () => {
+  const [positionX, setPositionX] = useState(0);
+  const [direction, setDirection] = useState(1);
+  const [isDancing, setIsDancing] = useState(false);
+  const [currentLocation, setCurrentLocation] = useState(0);
+  const [isEating, setIsEating] = useState(false); 
+  const step = 1;
 
-    const feedPet = () => {
-      setHunger((prevHunger) => (prevHunger > 10 ? prevHunger - 10 : 0));
+  const animateWalking = () => {
+    if (isDancing || isEating) return;
+
+    const newPositionX = positionX + step * direction;
+
+    if (newPositionX >= 250) {
+      setDirection(-1);
+    } else if (newPositionX <= 0) {
+      setDirection(1);
+    } else {
+      setPositionX(newPositionX);
+      setCurrentLocation(newPositionX);
+    }
+
+    requestAnimationFrame(animateWalking);
+  };
+
+  const startAutoDance = () => {
+    setIsDancing(true);
+    setTimeout(() => {
+      setIsDancing(false);
+      setIsEating(false);
+      requestAnimationFrame(animateWalking);
+    }, 2000);
+   
+  };
+
+  const startEatAnimation = () => {
+    setIsEating(true);
+    setTimeout(() => {
+      setIsEating(false);
+      setIsDancing(false);
+      requestAnimationFrame(animateWalking);
+    }, 3000); // Eat animation lasts for 3 seconds
+  };
+
+  useEffect(() => {
+    const animationId = requestAnimationFrame(animateWalking);
+
+    const autoDanceInterval = setInterval(() => {
+      startAutoDance();
+    }, 8000);
+
+    return () => {
+      cancelAnimationFrame(animationId);
+      clearInterval(autoDanceInterval);
     };
+  }, [positionX, direction]);
 
-    const playWithPet = () => {
-      setHappiness((prevHappiness) => (prevHappiness < 90 ? prevHappiness + 10 : 100));
-    };
-
-    const switchPet = () => {
-      setCurrentPet((prevPet) => (prevPet === "sprite" ? "gif" : "sprite"));
-    };
-
-    useEffect(() => {
-      let interval;
-      if (direction === "right") {
-        interval = setInterval(() => {
-          setPosition((prevPosition) => (prevPosition + 1) % 3);
-        }, 1000); // Adjust the interval duration to control the speed of movement
-      } else if (direction === "left") {
-        interval = setInterval(() => {
-          setPosition((prevPosition) => (prevPosition - 1 + 3) % 3);
-        }, 1000); // Adjust the interval duration to control the speed of movement
-      } else {
-        clearInterval(interval);
-      }
-
-      return () => clearInterval(interval);
-    }, [direction]);
-
-    useEffect(() => {
-      const timer = setInterval(() => {
-        const random = Math.random();
-        if (random < 0.33) {
-          setDirection("stand");
-        } else if (random < 0.67) {
-          setDirection("right");
-        } else {
-          setDirection("left");
-        }
-      }, 3000); // Adjust the interval duration to control the frequency of direction changes
-
-      return () => clearInterval(timer);
-    }, []);
-
-    return (
-      <div>
-        <Sidebar>
-          <main className="bg-gray-100 min-h-screen">
-            <Header title="Virtual Pet" />
-            <div className="p-4">
-              <h1 className="text-4xl font-bold mb-4">Virtual Pet</h1>
-              <div
-                className="flex items-center justify-center mb-8"
-                style={{ position: "relative", width: "200px", height: "100px" }}
-              >
-                <div
-                  style={{
-                    position: "absolute",
-                    left: `${position * 48}px`,
-                    top: "0",
-                    width: "1000px",
-                    height: "48px",
-                    transform: direction === "left" ? "scaleX(-1)" : "scaleX(1)",
-                  }}
-                >
-                  {currentPet === "sprite" ? (
-                    <Image
-                      src={
-                        direction === "stand"
-                          ? position === 0
-                            ? standSprite0
-                            : standSprite1
-                          : position === 0
-                          ? moveRightSprite4
-                          : moveRightSprite5
-                      }
-                      alt="Virtual Pet Sprite"
-                      layout="fill"
-                      objectFit="contain"
-                    />
-                  ) : (
-                    <img src={foxGif} alt="Virtual Pet GIF" style={{ width: "100%", height: "100%" }} />
-                  )}
-                </div>
-              </div>
-              <div className="flex justify-between">
-                <div>
-                  <p className="font-bold">Happiness</p>
-                  <div className="bg-gray-300 h-3 rounded-md">
-                    <div
-                      className="bg-green-400 h-full rounded-md"
-                      style={{ width: `${happiness}%` }}
-                    ></div>
-                  </div>
-                  <p className="text-xs mt-1">{happiness}%</p>
-                </div>
-                <div>
-                  <p className="font-bold">Hunger</p>
-                  <div className="bg-gray-300 h-3 rounded-md">
-                    <div
-                      className="bg-red-400 h-full rounded-md"
-                      style={{ width: `${hunger}%` }}
-                    ></div>
-                  </div>
-                  <p className="text-xs mt-1">{hunger}%</p>
-                </div>
-              </div>
-              <div className="mt-8 flex justify-center">
-                <button
-                  className="bg-green-500 text-white px-4 py-2 rounded-md mr-4"
-                  onClick={playWithPet}
-                >
-                  Play
-                </button>
-                <button
-                  className="bg-red-500 text-white px-4 py-2 rounded-md"
-                  onClick={feedPet}
-                >
-                  Feed
-                </button>
-                <button
-                  className="bg-blue-500 text-white px-4 py-2 rounded-md ml-4"
-                  onClick={switchPet}
-                >
-                  Switch Pet
-                </button>
-              </div>
-            </div>
-          </main>
-        </Sidebar>
+  return (
+    <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
+      <div style={{ width: "500px", height: "500px", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", border: "1px solid black" }}>
+        <img
+          src={isEating ? eatImage.src : isDancing ? addOilImage.src : direction === 1 ? rightwalkingImage.src : leftwalkingImage.src}
+          alt={isEating ? "Eating Image" : isDancing ? "Dancing Image" : "Walking Image"}
+          style={{ position: "absolute", top: "50%", left: positionX + "px", transform: "translateY(-50%)", width: "100px" }}
+        />
       </div>
-    );
-  }
+      <button onClick={() => alert(`Current Location: ${currentLocation}`)}>Show Current Location</button>
+      <button onClick={() => { startEatAnimation(); setIsDancing(false); }}>Eat and Stop</button>
+    </div>
+  );
+};
 
+export default WalkingAnimation;
 
