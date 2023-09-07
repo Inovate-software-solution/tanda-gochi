108d464375cb6ff398f0f0d79541549db05b4eb3
JOJOLion1224 <31367498+JOJOLion1224@users.noreply.github.com>
2023-09-08 09:48:17 +1000

    Reverting the merge

--- src/app/pet/page.tsx
+++ src/app/pet/page.tsx
@@ -3,20 +3,13 @@
 import Sidebar from "../../../components/Info/Sidebar";
 import Inventory from "../../../components/Pet/Inventory";
 import React, { useEffect, useState } from "react";
-import rightwalkingImage from "../../../public/images/walk.gif";
-import leftwalkingImage from "../../../public/images/walkleft.gif";
-import addOilImage from "../../../public/images/addoil.gif";
-import eatImage from "../../../public/images/eat.gif";
-import walkImageWithHat from "../../../public/images/walkhat.gif";
-import walkLeftImageWithHat from "../../../public/images/walklefthat.gif";
-import wearHatImage from "../../../public/images/wearhat.gif";
-import Image from "../../../public/images/food.jpg";
-
-const Page: React.FC = () => {
-  // Pet interaction related states
-  const [happiness, setHappiness] = useState(50);
-  const [fullness, setFullness] = useState(50);
-  //const [currentPet, setCurrentPet] = useState(50);
+import rightwalkingImage from "../walk.gif";
+import leftwalkingImage from "../walkleft.gif";
+import addOilImage from "../addoil.gif";
+import eatImage from "../eat.gif";
+import walkImageWithHat from "../walkhat.gif";
+import walkLeftImageWithHat from "../walklefthat.gif";
+import wearHatImage from "../wearhat.gif";
 
   // Pet animation related states
   const [positionX, setPositionX] = useState(0);
@@ -92,33 +85,6 @@ const Page: React.FC = () => {
     }, 4000); 
   };
 
-  const sections = [
-    {
-      title: 'Toys',
-      data: [
-        { image: 'toy_image_1.jpg', name: 'Toy 1' },
-        { image: 'toy_image_2.jpg', name: 'Toy 2' },
-        // Add more toy data here...
-      ],
-    },
-    {
-      title: 'Food',
-      data: [
-        { image: 'food_image_1.jpg', name: 'Food 1' },
-        { image: 'food_image_2.jpg', name: 'Food 2' },
-        // Add more food data here...
-      ],
-    },
-    {
-      title: 'Shop',
-      data: [
-        { image: 'shop_image_1.jpg', name: 'Shop 1' },
-        { image: 'shop_image_2.jpg', name: 'Shop 2' },
-        // Add more shop data here...
-      ],
-    },
-  ];
-
   useEffect(() => {
     const animationId = requestAnimationFrame(animateWalking);
 
@@ -161,109 +127,72 @@ const Page: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <Sidebar>
-          <main className="bg-gray-100 min-h-screen p-4">
-            <h1 className="text-4xl font-bold mb-4">Virtual Pet</h1>
-            <div className="flex flex-col justify-center items-center h-full">
-              <div className="relative bg-bg_pet bg-center bg-no-repeat flex justify-center items-center w-full" style={{ height: '50vh' }}>
-                <Inventory visibilityProp={isWindowVisible} titleProp={windowTitle} toggleProp={toggleWindow} typeProp={inventoryType} />
-                <div style={{ position: "relative", width: "100vw", height: "50vh", overflow: "hidden" }}>
-                  <div style={{ width: "500px", height: "500px", position: "absolute", top: "60%", left: "50%", transform: "translate(-50%, -50%)" }}>
-                    <img
-                        src={
-                            isEating
-                                ? eatImage.src
-                                : isDancing
-                                    ? addOilImage.src
-                                    : isWearingHat
-                                        ? wearHatImage.src
-                                        : isHat
-                                            ? direction === 1
-                                                ? walkImageWithHat.src
-                                                : walkLeftImageWithHat.src
-                                            : direction === 1
-                                                ? rightwalkingImage.src
-                                                : leftwalkingImage.src
-                        }
-                        alt={
-                            isEating
-                                ? "Eating Image"
-                                : isDancing
-                                    ? "Dancing Image"
-                                    : isWearingHat
-                                        ? "Wearing Hat Image"
-                                        : isHat
-                                            ? "Hat Walking Image"
-                                            : "Walking Image"
-                        }
-                        style={{ position: "relative", top: "50%", left: positionX + "px", transform: "translateY(-50%)", width: "100px" }}
-                    />
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-col md:flex-row justify-between w-full md:w-3/4 lg:w-1/2 mt-4 p-4 space-y-4 md:space-y-0 md:space-x-4">
-                  <div className="w-full md:w-1/3">
-                      <p className="font-bold">Happiness</p>
-                      <div className="bg-gray-300 h-3 rounded-md w-32">
-                          <div className="bg-green-400 h-full rounded-md" style={{ width: `${happiness}%` }}></div>
-                      </div>
-                      <p className="text-xs mt-1">{happiness}%</p>
-                  </div>
-
-                  <div className="w-full md:w-1/3">
-                      <p className="font-bold">Fullness</p>
-                      <div className="bg-gray-300 h-3 rounded-md w-32">
-                          <div className="bg-red-400 h-full rounded-md" style={{ width: `${fullness}%` }}></div>
-                      </div>
-                      <p className="text-xs mt-1">{fullness}%</p>
-                  </div>
-
-                  <div className="flex space-x-4 mt-4 md:mt-0">
-                      <button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={() => { toggleWindow("Actions and Toys", "toys"); playWithPet(); }}>
-                          Toys
-                      </button>
-                      <button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={() => { toggleWindow("Food", "food"); feedPet(); }}>
-                          Food
-                      </button>
-                      <button className="bg-yellow-500 text-white px-4 py-2 rounded-md" onClick={() => toggleWindow("Inventory", "costume")}>
-                          Costumes
-                      </button>
-                      <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={() => toggleWindow("Pets", "pet")}>
-                          Pets
-                      </button>
-                      <button className="bg-indigo-500 text-white px-4 py-2 rounded-md" onClick={() => toggleWindow("Shop", "shop")}>
-                          Shop
-                      </button>
-                  </div>
-              </div>
-
-              {/* <div className="grid grid-cols-3  ">
-                {sections.map((section, sectionIndex) => (
-                  <div key={sectionIndex} className="bg-blue-100 p-4 rounded-lg">
-                    <h2 className="text-xl font-semibold mb-2">{section.title}</h2>
-                    <div className="grid grid-cols-3 gap-2">
-                      {section.data.map((item, itemIndex) => (
-                        <div key={itemIndex} className="bg-white p-2 rounded-md">
-                          {item.image && (
-                            <img src={item.image} alt={item.name} />
-                          )}
-                          <p className="text-sm mt-1">{item.name}</p>
-                        </div>
-                      ))}
-                      
-                      {section.data.length < 9 && [...Array(9 - section.data.length)].map((_, emptyIndex) => (
-                        <div key={`empty-${emptyIndex}`} className="bg-white p-2 rounded-md" />
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div> */}
+    <Sidebar>
+      <main className="bg-gray-100 min-h-screen p-4">
+        
+        
+        <div className="relative bg-bg_pet bg-center bg-no-repeat flex flex-col items-center w-full">
+          {/* Pet Image */}
+          <div style={{ position: "relative", width: "100vw", height: "50vh", overflow: "hidden" }}>
+            <div style={{ width: "500px", height: "500px", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", border: "1px solid black" }}>
+              <img
+                src={
+                  isEating
+                    ? eatImage.src
+                    : isDancing
+                    ? addOilImage.src
+                    :isWearingHat
+                    ?wearHatImage.src
+                    :isHat
+                    ? direction === 1
+                      ? walkImageWithHat.src
+                      : walkLeftImageWithHat.src
+                    : direction === 1
+                    ? rightwalkingImage.src
+                    : leftwalkingImage.src
+                }
+                alt={
+                  isEating
+                    ? "Eating Image"
+                    : isDancing
+                    ? "Dancing Image"
+                    : isWearingHat
+                    ? "Wearing Hat Image"
+                    : isHat
+                    ? "Hat Walking Image"
+                    : "Walking Image"
+                }
+                style={{ position: "relative", top: "50%", left: positionX + "px", transform: "translateY(-50%)", width: "100px" }}
+              />
             </div>
-          </main>
-      </Sidebar>
-  </div>
+          </div>
+          
+          {/* Buttons */}
+          <div className="flex space-x-4 mt-4 md:mt-0">
+            <button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={() => {toggleWindow("Actions and Toys", "toys"); }}>
+              Toys
+            </button>
+            <button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={() => {toggleWindow("Food", "food"); }}>
+              Food
+            </button>
+            <button className="bg-yellow-500 text-white px-4 py-2 rounded-md" onClick={() => toggleWindow("Inventory", "costume")}>
+              Costumes
+            </button>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={() => { startEatAnimation(); setIsDancing(false); }}>Eat and Stop</button>
+             
+            <button className="bg-indigo-500 text-white px-4 py-2 rounded-md" onClick={() => toggleWindow("Shop", "shop")}>
+              Shop
+            </button>
+            <button className="bg-purple-500 text-white px-4 py-2 rounded-md" onClick={() => { startWearHatAnimation(); }}>
+    Wear
+  </button>
+          </div>
+          
+          {/* Inventory */}
+          <Inventory visibilityProp={isWindowVisible} titleProp={windowTitle} toggleProp={toggleWindow} typeProp={inventoryType} />
+        </div>
+      </main>
+    </Sidebar>
   );
 }
 
