4bb37ba3ce817d78cedd7391c1d74e35089cb14e
JOJOLion1224 <yxy624573403@gmail.com>
2023-09-19 20:17:06 +1000

    Improved the UI further and partially linked API

--- components/Pet/Inventory.jsx
+++ components/Pet/Inventory.jsx
@@ -3,6 +3,34 @@ import React, { useState, useEffect } from 'react';
 import Item from "./Item.jsx";
 
 const Inventory = (props) => {
+    const [userID, setUserID] = useState();
+    const [itemsToDisplay, setItemsToDisplay] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(true);
+    const [ error, setError ] = useState(null);
+
+    // Get the user's ID/Email here
+    
+    // Then use it to fetch their inventory
+    // useEffect(() => {
+    //     fetch(`https://capstone.marcusnguyen.dev/api/Users/${userID}`)
+    //     .then((res) => res.json())
+    //     .then(setItemsToDisplay(res.Inventory))
+    //     .catch(error => {
+    //         setError(error);
+    //     })
+    //     .finally(() => {
+    //         setIsLoading(false);
+    //     })
+    // }, [userID])
+
+    // 
+    // if (props.typeProp === 'shop') {
+    //     useEffect(() => {
+    //         fetch(`'https://capstone.marcusnguyen.dev/api/items'`)
+    //         .then("do something about it")
+    //     })
+    // }
+
     const [items, setItems] = useState([]);
     // mock items
     const type = {
@@ -41,60 +69,55 @@ const Inventory = (props) => {
         }, 2000); // Assuming the animation takes 2 seconds
     };
 
-    // <div className="grid grid-cols-3  ">
-    //     {sections.map((section, sectionIndex) => (
-    //         <div key={sectionIndex} className="bg-blue-100 p-4 rounded-lg">
-    //             <h2 className="text-xl font-semibold mb-2">{section.title}</h2>
-    //             <div className="grid grid-cols-3 gap-2">
-    //                 {section.data.map((item, itemIndex) => (
-    //                 <div key={itemIndex} className="bg-white p-2 rounded-md">
-    //                     {item.image && (
-    //                     <img src={item.image} alt={item.name} />
-    //                     )}
-    //                     <p className="text-sm mt-1">{item.name}</p>
-    //                 </div>
-    //                 ))}
-    //                 {/* Add empty boxes if there's no data */}
-    //                 {section.data.length < 9 && [...Array(9 - section.data.length)].map((_, emptyIndex) => (
-    //                 <div key={`empty-${emptyIndex}`} className="bg-white p-2 rounded-md" />
-    //                 ))}
-    //             </div>
-    //         </div>
-    //     ))}
-    // </div>
-
     return(
-        <div className="h-screen flex flex-col justify-center">
+        <div className="h-screen flex justify-center">
             {props.visibilityProp && (
                 <div 
-                    className="fixed bg-gray-50 p-8 rounded shadow-lg z-10" 
-                    style={{ top: '70%', left: '55%', transform: 'translate(-50%, -50%)', minWidth: '35vw', minHeight: '30vh' }}
+                    className="fixed bg-gray-50 p-4 rounded shadow-lg z-10" 
+                    style={{ top: '75%', left: '55%', transform: 'translate(-50%, -50%)', minWidth: '35vw', minHeight: '30vh' }}
                 >
-                    <div className="flex items-center justify-between">
-                        <h2 className="text-2xl mb-4">{props.titleProp}</h2>
+                    <div className="flex justify-between flex-col md:flex-row">
+                        <h2 className="text-base font-bold md:text-base lg:text-lg xl:text-xl mb-4">{props.titleProp}</h2>
                         {props.titleProp === "Shop" && (
-                            <div className="flex items-center">
-                                <img src='/images/coinGold.png' alt="Gold Coin" width={40} height={40} style={{ marginRight: '10px' }}/>
-                                <h3>Credits: {}</h3>
+                            <div className="sm:text-sm md:text-base flex items-center">
+                                    <img src='/images/coinGold.png' alt="Gold Coin" width={30} height={30} style={{ marginRight: '10px' }}/>  
+                                    Credits: {}
                             </div>
                         )}
-                        <button className="bg-pink-500 text-white px-3 rounded-md" onClick={props.toggleProp}>Close</button>
+                        <button className="btn btn-circle btn-outline"  onClick={props.toggleProp}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                        </button>
                     </div>
-                    
-                    <div className="p-4 overflow-y-auto h-96">
-                        <div className="grid grid-cols-4 gap-4 mt-6">
-                            {items.map(item => (
-                                <div key={item.id}>
-                                    <Item image={item.image} name={item.name}/>
-                                    {item.type === 'food' && (
-                                        <button onClick={() => { startEatAnimation(); setIsDancing(false); }}>
-                                            Eat and Stop
-                                        </button>
-                                    )}
-                                </div>
-                            ))}
-                        </div>
+
+                    {isLoading && (
+                        <div className='alert alert-warning mt-2'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                            <span>Loading Items...</span>
+                        </div> 
+                    )}
+
+                    {!isLoading && (
+                        <div className="mt-3 p-2 h-48 overflow-y-scroll bg-blue-100 rounded-lg grid grid-cols-4 gap-2 place-items-center">
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
+                        <Item></Item>
                     </div>
+                    )}
                 </div>
             )}
         </div>
--- src/app/pet/page.tsx
+++ src/app/pet/page.tsx
@@ -73,37 +73,6 @@ const Page: React.FC = () => {
     }, 2000);
   };
 
-  const startWearHatAnimation = () => {
-    if(isHat==false){setIsWearingHat(true);};
-    setTimeout(() => {
-      setIsDancing(false);
-      setIsEating(false);
-      setIsWearingHat(false);
-      toggleWearingHat();
-    }, 1000);
-  };
-
-  const startEatAnimation = () => {
-    setIsEating(true);
-    setTimeout(() => {
-      setIsEating(false);
-      setIsDancing(false);
-      setDirection(1);
-    requestAnimationFrame(animateWalking);
-    }, 4000); 
-  };
-
-  const startplayAnimation = () => {
-    setIsplaying(true);
-    setTimeout(() => {
-      setIsEating(false);
-      setIsDancing(false);
-      setIsplaying(false);
-      setDirection(1);
-    requestAnimationFrame(animateWalking);
-    }, 4000); 
-  };
-
   const feedPet = () => {
     setFullness((prevFullness) => (prevFullness < 90 ? prevFullness + 10 : 100));
   };
@@ -144,9 +113,9 @@ const Page: React.FC = () => {
     <div className="overflow-x-hidden" data-theme="emerald">
       <Sidebar>
           <main className="bg-gray-100 min-h-screen p-4">
-            <h1 className="text-4xl font-bold mb-4">Virtual Pet</h1>
+            {/* <h1 className="text-4xl font-bold mb-4">Virtual Pet</h1> */}
             <div className="flex flex-col justify-center items-center h-full">
-              <div className="relative bg-bg_pet bg-center bg-no-repeat flex justify-center items-center w-full" style={{ height: '50vh' }}>
+              <div className="border-4 border-sky-500 relative bg-bg_pet bg-center bg-no-repeat flex justify-center items-center" style={{ height: '50vh' }}>
                 <Inventory visibilityProp={isWindowVisible} titleProp={windowTitle} toggleProp={toggleWindow} typeProp={inventoryType} />
                 <div style={{ position: "relative", width: "100vw", height: "50vh", overflow: "hidden" }}>
                   <div style={{ width: "500px", height: "500px", position: "absolute", top: "60%", left: "50%", transform: "translate(-50%, -50%)" }}>
@@ -185,32 +154,32 @@ const Page: React.FC = () => {
                 </div>
               </div>
 
-            <div className="navbar bg-base-100 flex flex-col md:flex-row justify-center items-center md:w-3/4 lg:w-1/3 xl:w-3/4 mt-2 md:mt-4  md:p-4">
+            <div className="navbar bg-base-100 flex flex-col md:flex-row justify-center items-center md:w-3/4 lg:w-1/3 xl:w-3/4">
               <div className="w-full md:w-1/3">
                 <p className="font-bold mr-2">Happiness</p>
-                <progress className="progress progress-accent w-full md:w-2/3 lg:w-1/2 xl:w-56 mr-2" value={happiness} max="100"></progress>
+                <progress className="progress progress-accent w-full md:w-2/3 xl:w-56 mr-2" value={happiness} max="100"></progress>
                 <p className="text-xs mt-1">{happiness}%</p>
               </div>
 
               <div className="w-full md:w-1/3">
                 <p className="font-bold mr-2">Fullness</p>
-                <progress className="progress progress-success w-full md:w-2/3 lg:w-1/2 xl:w-56 mr-2" value={fullness} max="100"></progress>
+                <progress className="progress progress-success w-full md:w-2/3 xl:w-56 mr-2" value={fullness} max="100"></progress>
                 <p className="text-xs mt-1">{fullness}%</p>
               </div>
 
               <div className="flex space-x-4 mt-4 md:mt-0">
-                  <button className="btn btn-info px-4 py-2 rounded-md" onClick={() => { toggleWindow("Actions and Toys", "toys"); playWithPet(); }}>
-                      Toys
-                  </button>
-                  <button className="btn btn-success px-4 py-2 rounded-md" onClick={() => { toggleWindow("Food", "food"); feedPet(); }}>
-                      Food
-                  </button>
-                  <button className="btn btn-warning px-4 py-2 rounded-md" onClick={() => toggleWindow("Inventory", "costume")}>
-                      Costumes
-                  </button>
-                  <button className="btn btn-error px-4 py-2 rounded-md" onClick={() => toggleWindow("Shop", "shop")}>
-                      Shop
-                  </button>
+                <button className="btn btn-info px-2 md:px-4 py-2" onClick={() => { toggleWindow("Actions and Toys", "toys"); playWithPet(); }}>
+                    Toys
+                </button>
+                <button className="btn btn-primary px-2 px-4 py-2" onClick={() => { toggleWindow("Food", "food"); feedPet(); }}>
+                    Food
+                </button>
+                <button className="btn btn-warning px-2 px-4 py-2" onClick={() => toggleWindow("Inventory", "costume")}>
+                    Costumes
+                </button>
+                <button className="btn btn-error px-2 px-4 py-2" onClick={() => toggleWindow("Shop", "shop")}>
+                    Shop
+                </button>
               </div>
             </div>
           </div>
